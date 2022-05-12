cb35dc39bab1bbd71da763e5e7360065bea9a2b0
Martin Weindel <martin.weindel@sap.com>
2022-05-12 13:47:12 +0200

    add branch.cfg

--- charts/gardener-extension-shoot-networking-problemdetector/doc.go
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
-//
-// SPDX-License-Identifier: Apache-2.0
-
-//go:generate sh -c "../../vendor/github.com/gardener/gardener/hack/generate-controller-registration.sh shoot-networking-problemdetector . $(cat ../../VERSION) ../../example/controller-registration.yaml Extension:shoot-networking-problemdetector"
-
-// Package chart enables go:generate support for generating the correct controller registration.
-package chart
